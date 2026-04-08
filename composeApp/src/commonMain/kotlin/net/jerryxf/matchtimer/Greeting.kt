ffd32e42bf2bbd32491c07b77aa276440b4cd5be
Raphdf201 <raphdf201@hotmail.com>
2026-04-08 16:43:09 -0700

    Small cleanup

--- composeApp/src/commonMain/kotlin/net/jerryxf/matchtimer/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package net.jerryxf.matchtimer
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/net/jerryxf/matchtimer/Platform.kt
+++
@@ -1,7 +0,0 @@
-package net.jerryxf.matchtimer
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
