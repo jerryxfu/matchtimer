325f966a8e77b8eed66f373303cfb3db8261f660
Raphdf201 <raphdf201@hotmail.com>
2026-04-10 12:34:11 -0700

    Reorganize android ui

--- composeApp/src/androidMain/kotlin/net/jerryxf/matchtimer/LiveActivity.kt
+++ composeApp/src/androidMain/kotlin/net/jerryxf/matchtimer/LiveActivity.kt
@@ -1,67 +1,63 @@
 package net.jerryxf.matchtimer
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
-class LiveActivity {
-    private lateinit var notif: NotificationCompat.Builder
-    private val notifStyle = NotificationCompat.ProgressStyle()
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(20))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(3))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(10))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
-        .addProgressSegment(NotificationCompat.ProgressStyle.Segment(30))
-    private val notifId = ++notifCount
+@SuppressLint("StaticFieldLeak")
+private lateinit var notif: NotificationCompat.Builder
+private const val CHANNEL_ID = "LiveActivity"
+private val notifStyle = NotificationCompat.ProgressStyle()
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(20))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(3))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(10))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(25))
+    .addProgressSegment(NotificationCompat.ProgressStyle.Segment(30))
 
-    fun start() {
-        notif = NotificationCompat.Builder(MainActivity.context, CHANNEL_ID)
-            .setContentTitle("Match")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setRequestPromotedOngoing(true)
-            .setOngoing(true)
-            .setShowWhen(false)
-            .setStyle(notifStyle.setProgress(0))
+fun MainActivity.startLiveActivity(context: Context) {
+    notif = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setContentTitle("Match")
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setRequestPromotedOngoing(true)
+        .setOngoing(true)
+        .setShowWhen(false)
+        .setStyle(notifStyle.setProgress(0))
 
-        update()
-    }
+    update(context)
+}
 
-    private fun update() = with(NotificationManagerCompat.from(MainActivity.context)) {
-        if (ActivityCompat.checkSelfPermission(
-                MainActivity.context,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) ActivityCompat.requestPermissions(MainActivity.instance, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
-        notify(notifId, notif.build())
-    }
+private fun MainActivity.update(context: Context) = with(NotificationManagerCompat.from(context)) {
+    if (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) ActivityCompat.requestPermissions(this@update, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
+    notify(1, notif.build())
+}
 
-    fun setProgress(state: MatchState) {
-        notif.setStyle(notifStyle.setProgress((state.totalElapsed * 100) / MatchState.MATCH_DURATION))
-        notif.setShortCriticalText(state.timeString())
-        update()
-    }
+fun MainActivity.setActivityProgress(state: MatchState, context: Context) {
+    notif.setStyle(notifStyle.setProgress((state.totalElapsed * 100) / MatchState.MATCH_DURATION))
+    notif.setShortCriticalText(state.timeString())
+    update(context)
+}
 
-    companion object {
-        private var notifCount = 0
-        private const val CHANNEL_ID = "LiveActivity"
-
-        init {
-            (MainActivity.context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
-                .createNotificationChannel(
-                    NotificationChannel(
-                        CHANNEL_ID,
-                        "Live activities",
-                        NotificationManager.IMPORTANCE_HIGH
-                    )
-                )
-        }
-    }
+fun initNotificationChannel(context: Context) {
+    (context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
+        .createNotificationChannel(
+            NotificationChannel(
+                CHANNEL_ID,
+                "Live activities",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+        )
 }
--- composeApp/src/androidMain/kotlin/net/jerryxf/matchtimer/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/net/jerryxf/matchtimer/MainActivity.kt
@@ -1,31 +1,46 @@
 package net.jerryxf.matchtimer
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
-    companion object {
-        @SuppressLint("StaticFieldLeak")
-        lateinit var context: Context
-            private set
-
-        @SuppressLint("StaticFieldLeak")
-        lateinit var instance: MainActivity
-            private set
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        context = applicationContext
-        instance = this
 
         val requestPermissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()
@@ -36,6 +51,69 @@ class MainActivity : ComponentActivity() {
 
         requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
 
-        setContent { App() }
+        initNotificationChannel(applicationContext)
+
+        setContent {
+            App()
+        }
     }
+
+    @Preview
+    @Composable
+    private fun App() {
+        val dark = isSystemInDarkTheme()
+        var timer by remember { mutableStateOf(MatchTimer(null)) }
+        val coroutineScope = rememberCoroutineScope()
+
+        MaterialTheme {
+            Surface(
+                Modifier.fillMaxSize(),
+                color = if (dark) Color(30, 31, 34) else Color.White,
+                contentColor = if (dark) Color.White else Color.Black
+            ) {
+                Column(
+                    Modifier.fillMaxSize(),
+                    Arrangement.Top,
+                    Alignment.CenterHorizontally
+                ) {
+                    Spacer(Modifier.height(50.dp))
+
+                    Text(timer.matchState.collectAsState().value.timeString(), fontSize = 50.sp)
+                    Row(Modifier.fillMaxWidth(), Arrangement.SpaceEvenly) {
+                        Spacer(Modifier.width(10.dp))
+                        Button({
+                            timer.start(coroutineScope) { setActivityProgress(it, applicationContext) }
+                            startLiveActivity(applicationContext)
+                        }, Modifier.weight(.5f), colors = ButtonDefaults.buttonColors(containerColor = Color.Green)) {
+                            Text("Start")
+                        }
+                        Spacer(Modifier.width(10.dp))
+                        Button({ timer.stop() }, Modifier.weight(.5f),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                            Text("Stop")
+                        }
+                        Spacer(Modifier.width(10.dp))
+                    }
+
+                    Column(Modifier.scrollable(rememberScrollState(), Orientation.Vertical)) {
+                        Text("Auto")
+                        Text("Pause")
+                        Text("Transition")
+                        Text("Shift 1")
+                        Text("Shift 2")
+                        Text("Shift 3")
+                        Text("Shift 4")
+                        Text("Endgame")
+                        Text("Match over")
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun MatchState.timeString(): String {
+    val m = this.totalSecondsRemaining / 60
+    val s = this.totalSecondsRemaining % 60
+    return String.format("%d:%02d", m, s)
 }
